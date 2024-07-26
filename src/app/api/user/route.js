6fd6c652c7817197aa7263b3b64c1d5d6452b55d
chatsadawut <azathos000@gmail.com>
2024-07-26 12:49:42 +0700

    ttttt

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -6,6 +6,7 @@ dotenv.config();
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
+client.connect();
 //-------------------------------------------------------------------------------------
 export async function GET() {
   try {
@@ -27,13 +28,37 @@ export async function GET() {
 
 export async function POST(request) {
   try {
-  const { firstname, lastname, username, password } = await request.json();
+  const {id, firstname, lastname, username, password } = await request.json();
   // Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log(hashedPassword);
-  const res = await client.query('INSERT INTO tbl_users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4) RETURNING *', [firstname, lastname, username, hashedPassword]);
+  const res = await client.query('INSERT INTO tbl_users (id, firstname, lastname, username, password) VALUES ($1, $2, $3, $4, $5) RETURNING *', [ id, firstname, lastname, username, hashedPassword]);
   return new Response(JSON.stringify(res.rows[0]), {
   status: 201,
+  headers: { 'Content-Type': 'application/json' },
+  });
+  } catch (error) {
+  console.error(error);
+  return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+  status: 404,
+  headers: { 'Content-Type': 'application/json' },
+  });
+  }
+  }
+//-------------------------------------------------------------------------------------
+
+export async function PUT(request) {
+  try {
+  const { id, firstname, lastname } = await request.json();
+  const res = await client.query('UPDATE tbl_users SET firstname = $1, lastname = $2 WHERE id = $3 RETURNING *', [firstname, lastname, id]);
+  if (res.rows.length === 0) {
+  return new Response(JSON.stringify({ error: 'User not found' }), {
+  status: 404,
+  headers: { 'Content-Type': 'application/json' },
+  });
+  }
+  return new Response(JSON.stringify(res.rows[0]), {
+  status: 200,
   headers: { 'Content-Type': 'application/json' },
   });
   } catch (error) {
@@ -44,36 +69,27 @@ export async function POST(request) {
   });
   }
   }
-
 //-------------------------------------------------------------------------------------
-
-export async function PUT() {
+export async function DELETE(request) {
   try {
-    return new Response(JSON.stringify({ message: "PUT DATA OK"}), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+  const { id } = await request.json();
+  const res = await client.query('DELETE FROM tbl_users WHERE id = $1 RETURNING *', [id]);
+  if (res.rows.length === 0) {
+  return new Response(JSON.stringify({ error: 'User not found' }), {
+  status: 404,
+  headers: { 'Content-Type': 'application/json' },
+  });
   }
-}
-//-------------------------------------------------------------------------------------
-export async function DELETE() {
-  try {
-    return new Response(JSON.stringify({ message: "DELETE DATA OK"}), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
+  return new Response(JSON.stringify(res.rows[0]), {
+  status: 200,
+  headers: { 'Content-Type': 'application/json' },
+  });
   } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+  console.error(error);
+  return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+  status: 500,
+  headers: { 'Content-Type': 'application/json' },
+  });
   }
-}
+  }
 //-------------------------------------------------------------------------------------
